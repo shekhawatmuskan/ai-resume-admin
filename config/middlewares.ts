e0d96fbbbffe66baeea7c2cebc3700c6cc2100b0
shekhawatmuskan <muskanshekhawat73@gmail.com>
2026-02-17 12:02:47 +0530

    updated middleware.js

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,9 +8,10 @@ export default [
 
       origin: [
         'http://localhost:5173',
+        'http://localhost:5174',
         'https://ai-resume-buildr.netlify.app',
         'https://ai-resume-admin-9iqm.onrender.com',
-        '*', 
+        '*',
       ],
     },
   },
